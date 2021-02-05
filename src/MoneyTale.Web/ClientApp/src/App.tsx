e8784d60209aa13247cd1afe6b8fc5b2f4cdfa00
Marcin Kotlicki <kotlickimarcin@gmail.com>
2021-02-06 00:52:50 +0100

    Configuring prettier, cleaning up project

--- src/MoneyTale.Web/ClientApp/src/App.tsx
+++ src/MoneyTale.Web/ClientApp/src/App.tsx
@@ -2,19 +2,12 @@ import React from "react";
 import { Route } from "react-router";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
-import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
 import ApiAuthorizationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
 import { ApplicationPaths } from "./components/api-authorization/ApiAuthorizationConstants";
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-
-import "./custom.css";
 
 const App = () => (
   <Layout>
-    <Route path="/" innerComponent={Home} />
-    <Route path="/counter" component={Counter} />
-    <AuthorizeRoute path="/fetch-data" component={FetchData} />
+    <Route path="/" component={Home} />
     <Route
       path={ApplicationPaths.ApiAuthorizationPrefix}
       component={ApiAuthorizationRoutes}
