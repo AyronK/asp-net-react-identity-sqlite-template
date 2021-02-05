96d7ee05e0103fe9f81d2b7ad5026d18ae3bdb11
Marcin Kotlicki <kotlickimarcin@gmail.com>
2021-02-06 00:06:45 +0100

    Added typescript and eslint

---
+++ src/MoneyTale.Web/ClientApp/src/components/NavMenu.tsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { LoginMenu } from "./api-authorization/LoginMenu";
+
+import "./NavMenu.css";
+
+interface NavMenuState {
+  collapsed: boolean;
+}
+
+class NavMenu extends Component<any, NavMenuState> {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true,
+    };
+  }
+
+  toggleNavbar() {
+    this.setState((s) => ({
+      collapsed: !s.collapsed,
+    }));
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar
+          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+          light
+        >
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              MoneyTale.Web
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse
+              className="d-sm-inline-flex flex-sm-row-reverse"
+              isOpen={!this.state.collapsed}
+              navbar
+            >
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">
+                    Home
+                  </NavLink>
+                </NavItem>
+                <LoginMenu />
+              </ul>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
+
+export default NavMenu;
